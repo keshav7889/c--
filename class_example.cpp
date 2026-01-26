9502ec9df5454444075801c4864e1cd6249e7208
keshav7889 <keshavaggarwal2007@gmail.com>
2026-01-26 10:57:21 +0530

    example for defining class in c++

---
+++ class_example.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class person{
+char name[50];
+int age;
+public:
+void getdata(void);
+void display(void);
+};
+void person :: getdata(void)
+{
+ cout<<" Enter Name: ";
+ cin>>name;
+ cout<<" Enter Age: ";
+ cin>>age;
+
+}
+void person :: display(void){
+cout<<"\nName : "<<name;
+cout<<"\nage  : "<<age;
+}
+int main(){
+person p;
+p.getdata();
+p.display();
+
+return 0;
+
+}
